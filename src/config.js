503b9a1662e529f4b9dcb099f5ca224d497c06cf
liviu.ignat <liviu.ignat@check24.de>
2015-09-09 17:21:13 +0200

    Remove unused deps

--- src/config.js
+++ src/config.js
@@ -15,11 +15,8 @@ System.config({
   "map": {
     "babel": "npm:babel-core@5.7.4",
     "babel-runtime": "npm:babel-runtime@5.7.0",
-    "backbone": "npm:backbone@1.2.1",
     "core-js": "npm:core-js@0.9.18",
     "jquery": "github:components/jquery@2.1.4",
-    "jquery-ui": "github:components/jqueryui@1.11.4",
-    "moment": "github:moment/moment@2.10.3",
     "text": "github:systemjs/plugin-text@0.0.2",
     "underscore": "npm:underscore@1.8.3",
     "github:components/jqueryui@1.11.4": {
@@ -30,10 +27,6 @@ System.config({
     },
     "npm:babel-runtime@5.7.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:backbone@1.2.1": {
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "underscore": "npm:underscore@1.8.3"
     },
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
