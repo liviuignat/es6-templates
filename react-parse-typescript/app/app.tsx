edf2d5dceffd2bd1bb4e9e0ed52436c329da7f0f
liviu.ignat <liviu.ignat@check24.de>
2015-10-06 15:14:00 +0200

    React parse config

--- react-parse-typescript/app/app.tsx
+++ react-parse-typescript/app/app.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import parseConfig from './config/parse.config';
+import Parse from 'parse';
 import AppRouter from './components/AppRouter';
 
 window.React = React;
-parseConfig.init();
+
+Parse.initialize('HrMPFQFNyOPjq8cR9i67xSyAzAggfJYwTetpDUwB', 'Pm8doOztn0N8iXfNzisX5RrV4r2y1wbbTKHxRoUr');
 
 const appNode = window.document.getElementById('app');
 React.render(<AppRouter />, appNode);
