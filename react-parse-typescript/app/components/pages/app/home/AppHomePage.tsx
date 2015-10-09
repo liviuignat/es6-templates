82c06bbccab5bd33442324acd3ea041614f4ed24
Liviu Ignat <liviu@ignat.email>
2015-10-10 00:37:39 +0200

    playing with menu

--- react-parse-typescript/app/components/pages/app/home/AppHomePage.tsx
+++ react-parse-typescript/app/components/pages/app/home/AppHomePage.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { List, ListItem } from 'material-ui/lib/lists';
 
 export default class extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
+    
+    this.props.menuStyle = {
+       width: '100px',
+       marginLeft: '-16px',
+       marginTop: '-13px',
+    }
   }
 
   render() {
     return (
       <div>
-        <h3>Hello logged in user</h3>
+        <List style={ this.props.menuStyle }>
+          <ListItem primaryText='Dashboard' />
+          <ListItem primaryText={Layouts} />
+          <ListItem primaryText='Languages'/>
+        </List>
       </div>
     );
   }
